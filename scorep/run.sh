bd874598ff1c1c1019588b3a60fe0b81d510ac03
GozDavid <dgoz@amonra.hpc.hotcat>
2024-06-03 14:42:50 +0200

    scorep script update

--- scorep/run.sh
+++ scorep/run.sh
@@ -4,43 +4,74 @@
 # to compile and perform code profiling and
 # tracing using Score-p tool
 
-###################################### TO EDIT ######################################################
-# set the compiler
-COMPILER=mpicc
+if [ ${PROFILING} -eq 0 ] && [ ${TRACING} -eq 0 ]
+then
+    printf "\n\t PROFILING or TRACING \n"
+    exit 0
+fi
 
-# set the machine name on the src/Makefile accordingly
-SYSTEM=LeonardoBoost
+if [ ${PROFILING} -eq 1 ] && [ ${TRACING} -eq 1 ]
+then
+    printf "\n\t PROFILING or TRACING exclusively\n"
+    exit 1
+fi
 
-# executable basename
-BASE=Pinocchio
-# executable with/without OMP or Debug support
-EXEC=( "${BASE}" "${BASE}OMP" )
+if [ ${#COMPILER} -eq 0 ]
+then
+    printf "\n\t Set the COMPILER ...aborting...\n"
+    exit 2
+fi
 
-# Select the PAPI metrics (empty means do not use PAPI counters)
-# PAPI_METRIC=PAPI_DP_OPS,PAPI_L3_TCM
-PAPI_METRIC=
+if [ ${#SYSTEM} -eq 0 ]
+then
+    printf "\n\t Set the SYSTEM ...aborting...\n"
+    exit 3
+fi
 
-# Number of MPI tasks (array)
-NTASKS=( 8 )
+if [ ${#EXEC[@]} -eq 0 ]
+then
+    printf "\n\t EXEC empty ...aborting...\n"
+    exit 4
+fi
 
-# MPI mapping list (e.g. socket, numa, core)
-MPI_MAP_BY=( numa )
+if [ ${#NTASKS[@]} -eq 0 ]
+then
+    printf "\n\t NTASKS empty ...aborting...\n"
+    exit 5
+fi
 
-# Number of OMP threads per MPI task (array)
-OMP_THR=( 1 )
+if [ ${#MPI_MAP_BY[@]} -eq 0 ]
+then
+    printf "\n\t MPI_MAP_BY empty ...aborting...\n"
+    exit 6
+fi
 
-# Parameter file
-PARAMFILE=${WORKDIR}/example/parameter_file_profiling
+if [ ${#OMP_THR[@]} -eq 0 ]
+then
+    printf "\n\t OMP_THR empty ...aborting...\n"
+    exit 7
+fi
 
-# Outputs (name of file with required output redshifts)
-OUTPUTS=${WORKDIR}/example/outputs_scorep
+if [ ! -f ${PARAMFILE} ]
+then
+    printf "\n\t Paramfile: ${PARAMFILE} not found ...aborting...\n"
+    exit 8
+fi
 
-# Profiling/tracing directory (output directory)
-OUT_DIR=${WORKDIR}/profiling_256_par_256_box
+if [ ! -f ${OUTPUTS} ]
+then
+    printf "\n\t Outputs: ${OUTPUTS} not found ...aborting...\n"
+    exit 9
+fi
 
-####################################### END TO EDIT #################################################
-#####################################################################################################
-#####################################################################################################
+if [ "${OUT_DIR}" = "${WORKDIR}/scorep" ] || [ "${OUT_DIR}" = "${WORKDIR}/src" ] || [ "${OUT_DIR}" = "${WORKDIR}/example" ]
+then
+    printf "\n\t Invalid output directory: ${OUT_DIR} ...aborting..."
+    exit 10
+else
+    # create the directory. If already exists then keep files
+    mkdir -p ${OUT_DIR}
+fi
 
 # Number of nodes (set automatically by Slurm)
 if [ ${#SLURM_NNODES} -eq 0 ]
@@ -48,26 +79,4 @@ then
     NODES=1
 else
     NODES=${SLURM_NNODES}
-fi
-
-# check
-if [ ! -f ${PARAMFILE} ]
-then
-    printf "\n\t Paramfile: ${PARAMFILE} not found ...aborting...\n"
-    exit 0
-fi
-
-if [ ! -f ${OUTPUTS} ]
-then
-    printf "\n\t Outputs: ${OUTPUTS} not found ...aborting...\n"
-    exit 1
-fi
-
-if [ "${OUT_DIR}" = "${WORKDIR}/scorep" ] || [ "${OUT_DIR}" = "${WORKDIR}/src" ] || [ "${OUT_DIR}" = "${WORKDIR}/example" ]
-then
-    printf "\n\t Invalid output directory: ${OUT_DIR} ...aborting..."
-    exit 2
-else
-    # create the directory. If already exists then keep files
-    mkdir -p ${OUT_DIR}
 fi
